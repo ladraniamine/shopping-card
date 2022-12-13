236e8927d6ed562ab561b4878afcb2c930aff8e5
Your Name <ladraniful10@gmail.com>
2022-12-13 17:15:05 +0100

    remove loginSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dataSlice from "./dataSlice";
-import loginslice from "./loginslice";
 import authSlice from "./authSlice";
 import cardSlice from "./cardSlice";
 
 const store = configureStore({
 
-    reducer:{login:loginslice,
+    reducer:{
              data:dataSlice,
              auth:authSlice,
              card:cardSlice,
-            
             }
 
 })
