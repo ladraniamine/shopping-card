ec91ac00865a07be0b5b63f15c6d3a19aed2676b
Your Name <ladraniful10@gmail.com>
2022-12-15 00:05:12 +0100

    check if inputs are empty

--- src/component/register.js
+++ src/component/register.js
@@ -1,45 +1,77 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Register = () => {
-const fullname = useRef()
-const email = useRef()
-const password = useRef()
+  const fullname = useRef()
+  const email = useRef()
+  const password = useRef()
+  
+  const [isfullname,setisfullname] = useState(false)
+  const [isemail,setisemail] = useState(false)
+  const [ispassword,setispassword] = useState(false)
 
-const handlesubmit = ()=>{
-  const FullName = fullname.current.value
-  const Email = email.current.value
-  const Password = password.current.value
-  if(FullName === "" && Email === "" && Password === ""){
-    console.log("fullname and email and password are empty")
-  }else if(FullName === "" && Email === "" && Password != ""){
-      console.log("fullname and email are empty")
-  }else if(FullName === "" && Email != "" && Password === ""){
-    console.log("fullname and password are empty")
-  }else if(FullName != "" && Email === "" && Password === ""){
-    console.log("email and password are empty")
-  }else if (FullName === "" && Email != "" && Password != ""){
-    console.log("fullname is empty")
-  }else if (FullName != "" && Email === "" && Password != ""){
-    console.log("email is empty")
-  }else if(FullName != "" && Email != "" && Password === ""){
-    console.log("password is empty")
-  }else{
-    console.log("login")
-  }
+  const handlesubmit = ()=>{
+    const FullName = fullname.current.value
+    const Email = email.current.value
+    const Password = password.current.value
+
+    if(FullName === "" && Email === "" && Password === ""){
+      console.log("fullname and email and password are empty")
+      setisfullname(true)
+      setisemail(true)
+      setispassword(true)
+    }else if(FullName === "" && Email === "" && Password != ""){
+        console.log("fullname and email are empty")
+        setisfullname(true)
+      setisemail(true)
+      setispassword(false)
+    }else if(FullName === "" && Email != "" && Password === ""){
+      console.log("fullname and password are empty")
+      setisfullname(true)
+      setisemail(false)
+      setispassword(true)
+    }else if(FullName != "" && Email === "" && Password === ""){
+      console.log("email and password are empty")
+      setisfullname(false)
+      setisemail(true)
+      setispassword(true)
+    }else if (FullName === "" && Email != "" && Password != ""){
+      console.log("fullname is empty")
+      setisfullname(true)
+      setisemail(false)
+      setispassword(false)
+    }else if (FullName != "" && Email === "" && Password != ""){
+      console.log("email is empty")
+      setisfullname(false)
+      setisemail(true)
+      setispassword(false)
+    }else if(FullName != "" && Email != "" && Password === ""){
+      console.log("password is empty")
+      setisfullname(false)
+      setisemail(false)
+      setispassword(true)
+    }else{
+      console.log("login")
+      setisfullname(false)
+      setisemail(false)
+      setispassword(false)
+    }
 }
 
   return (
     <div className='row' style={{"width":"100vw","height":"100vh"}}>
       <div className=' row m-auto '>
           <div className='col-12 text-center'>
-            <input ref={fullname} className='form-control m-auto' placeholder='full name' style={{"width":"200px"}} />
+            <input ref={fullname} className={isfullname?"form-control m-auto border-danger":"form-control m-auto"} placeholder='full name' style={{"width":"200px"}} />
+            {isfullname?<div className='text-danger empty-message'>you should enter the fullname</div>:""}
           </div>
           <div className='col-12 text-center mt-2'>
-            <input ref={email} className='form-control m-auto' type="email" placeholder='email' style={{"width":"200px"}} />
+            <input ref={email} className={isemail?"form-control m-auto border-danger":"form-control m-auto"} type="email" placeholder='email' style={{"width":"200px"}} />
+           {isemail? <div className='text-danger empty-message'>you should enter the email</div>:""}
           </div>
           <div className='col-12 text-center mt-2'>
-            <input ref={password} className='form-control m-auto' type="password" placeholder='password' style={{"width":"200px"}} />
+            <input ref={password} className={ispassword?"form-control m-auto border-danger":"form-control m-auto"} type="password" placeholder='password' style={{"width":"200px"}} />
+            {ispassword?<div className='text-danger empty-message'>you should enter the password</div>:""}
           </div>
           <div className='col-12 text-center mt-2'>
             <NavLink to="/register">
